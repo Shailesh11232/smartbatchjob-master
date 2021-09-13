28820dd10750dba550eac4da82a1e8e5c9d7c9ed
Shailesh11232 <78489852+Shailesh11232@users.noreply.github.com>
2021-09-13 10:45:13 +0530

    Initial commit

---
+++ DataBase.sql
@@ -0,0 +1,32 @@
+select * from smartbatchjobui.create_batch_job;
+select * from smartbatchjobui.create_batch_job_parameter;
+
+drop table smartbatchjobui.create_batch_job;
+drop table smartbatchjobui.create_batch_job_parameter;
+
+create table smartbatchjobui.create_batch_job(
+			id int AUTO_increment,
+            Batch_Job_Name varchar(25),
+            Batch_Job_Description varchar(25),
+            Batch_Job_Type varchar(25),
+            audit_create_date DATETIME,
+			audit_update_date DATETIME,
+            primary key (id)
+            );
+
+create table smartbatchjobui.create_batch_job_parameter(
+			id int AUTO_increment,
+            Parameter_Name varchar(50),
+			Parameter_Description varchar(25),
+			Parameter_Type  varchar(25),
+			Parameter_Format  varchar(25),
+			Mandatory_Flag Boolean,
+			Visible_Flag Boolean,
+			Default_Value varchar(25),
+			Regex_for_Validation varchar(25),
+            audit_create_date DATETIME,
+		    audit_update_date DATETIME,
+            primary key (id)
+            );
+            
+            
---
+++ SpringBoot/smartbatchjobui/smartbatchjobui/src/main/java/com/smartbatchjobui/smartbatchjobui/resources/CreateBatchJobResources.java
@@ -0,0 +1,64 @@
+package com.smartbatchjobui.smartbatchjobui.resources;
+
+import com.smartbatchjobui.smartbatchjobui.dto.CreateBatchJob;
+import com.smartbatchjobui.smartbatchjobui.dto.CreateBatchJobParameter;
+import com.smartbatchjobui.smartbatchjobui.services.CreateBatchJobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+@SpringBootApplication
+//@CrossOrigin("*")
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api")
+public class CreateBatchJobResources {
+
+    @Autowired
+    private CreateBatchJobService createBatchJobService;
+
+      @PostMapping("cbj")
+      ResponseEntity<CreateBatchJob>  CreateBJob(@RequestBody CreateBatchJob createBatchJob){
+          System.out.println("CreateBatchJob:"+createBatchJob);
+          CreateBatchJob createBatchJobResult =
+                  createBatchJobService.CreateBJob(createBatchJob);
+          URI location =
+                  ServletUriComponentsBuilder.fromCurrentRequestUri()
+                          .path("{/id}").buildAndExpand(createBatchJobResult).toUri();
+          return ResponseEntity.created(location).body(createBatchJobResult);
+    }
+    @DeleteMapping("/ParameterDelete/{id}")
+    void DeleteParameter(@PathVariable("id")Long id){
+        createBatchJobService.deleteById(id);
+    }
+    @PutMapping("/UpdateParameter/{id}")
+    ResponseEntity<CreateBatchJobParameter> UpdateParameter(@RequestBody CreateBatchJobParameter createBatchJobParameter,
+                                                             @PathVariable("id")Long id){
+        System.out.println("CreateBatchJobParameter:"+createBatchJobParameter);
+        CreateBatchJobParameter createBatchJobParameterResult =
+                createBatchJobService.UpadetParameter(createBatchJobParameter, id);
+        URI location =
+                ServletUriComponentsBuilder.fromCurrentRequestUri()
+                        .path("{/id}").buildAndExpand(createBatchJobParameterResult).toUri();
+        return ResponseEntity.created(location).body(createBatchJobParameterResult);
+    }
+    @GetMapping("/cbj/{createBatchJobId}")
+    ResponseEntity<List<CreateBatchJobParameter>> getIdBatchJob(@PathVariable(
+            "createBatchJobId")Long createBatchJobId){
+        System.out.println("CreateBatchJobId:"+createBatchJobId);
+        List<CreateBatchJobParameter> createBatchJobParametersResult =
+                createBatchJobService.getIdBatchJob(createBatchJobId);
+        return ResponseEntity.ok().body(createBatchJobParametersResult);
+    }
+    @GetMapping( "/cbj/getAll")
+    ResponseEntity<List<CreateBatchJob>> getAll() {
+        List<CreateBatchJob> createBatchJobResult =
+                createBatchJobService.getAllBatchJob();
+        return ResponseEntity.ok().body(createBatchJobResult);
+    }
+    
+}
